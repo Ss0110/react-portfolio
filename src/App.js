b31bc42be489705aaf18b89a4ed6945880b4b8d1
Ss0110 <saraysanchez01@yahoo.com>
2023-05-31 11:01:48 -0700

    added necessary files

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import AboutMe from "./components/AboutMe";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navigation />
+      <main>
+        <AboutMe />
+        <Portfolio />
+        <Contact />
+        <Resume />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>&copy; 2023 Saray Sanchez</p>
+      </div>
+      <div>
+        <a href="http://github.com/Ss0110">GitHub</a>
+        <a href="https://www.linkedin.com/in/saray-sanchez-a26251259/">
+          LinkedIn
+        </a>
+        <a href="mailto:saraysanchez01@yahoo.com">Email</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
